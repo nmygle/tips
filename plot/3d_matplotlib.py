73b91e2b22a179f60cab5a76978e18d6ff147d22
nmygle <33917290+nmygle@users.noreply.github.com>
2022-01-31 14:41:21 +0900

    Create 3d_matplotlib.py

---
+++ plot/3d_matplotlib.py
@@ -0,0 +1,13 @@
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot3d(x, y, z, title=None):
+    fig = plt.figure(figsize=plt.figaspect(1))
+    ax = Axes3D(fig)
+    ax.scatter3D(x, y, z, s=1)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    if title is None:
+        ax.set_title(title)
+    ax.view_init(190, 50)
+    plt.show()
